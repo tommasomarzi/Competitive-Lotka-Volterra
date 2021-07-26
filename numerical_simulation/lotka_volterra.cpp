5fa716d6470161ae8e933432b64e196632edbac6
Tommaso Marzi <marzitommaso@gmail.com>
2021-07-26 10:58:42 +0200

    Added line-by-line steps in the runge-kutta method

--- numerical_simulation/lotka_volterra.cpp
+++ numerical_simulation/lotka_volterra.cpp
@@ -40,36 +40,36 @@ vector<double> runge_kutta(vector<double> values, vector<double> rate, vector<ve
     vector<double> result;  
     for(i = 0; i < values.size(); i++)
     {
-       tmp1.push_back(lotka_volterra(i, values, rate[i], interaction[i]));      //f(x,t)      
+       tmp1.push_back(lotka_volterra(i, values, rate[i], interaction[i]));      //f(x(0),t)      
     }
     fields.push_back(tmp1);
-    tmp2 = VectorPlusVector(values, VectorTimesScalar(tmp1,h_half));            //y 
+    tmp2 = VectorPlusVector(values, VectorTimesScalar(tmp1,h_half));            //y = x(0) + f(x(0),t)*h/2
     tmp1.clear();
 
     for(i = 0; i < values.size(); i++)
         tmp1.push_back(lotka_volterra(i, tmp2, rate[i], interaction[i]));       //f(y,t) 
     fields.push_back(tmp1);  
-    tmp2 = VectorPlusVector(values, VectorTimesScalar(tmp1,h_half));            //z
+    tmp2 = VectorPlusVector(values, VectorTimesScalar(tmp1,h_half));            //z = x(0) + f(y,t)*h/2
     tmp1.clear();
 
     for(i = 0; i < values.size(); i++)
         tmp1.push_back(lotka_volterra(i, tmp2, rate[i], interaction[i]));       //f(z,t) 
 
     fields.push_back(tmp1);  
-    tmp2 = VectorPlusVector(values, VectorTimesScalar(tmp1,h_increment));       //w
+    tmp2 = VectorPlusVector(values, VectorTimesScalar(tmp1,h_increment));       //w = x(0) + f(z,t)*h
     tmp1.clear();
 
     for(i = 0; i < values.size(); i++)
         tmp1.push_back(lotka_volterra(i, tmp2, rate[i], interaction[i]));       //f(w,t) 
     fields.push_back(tmp1);  
     
-    fields[1] = VectorTimesScalar(fields[1], 2);
-    fields[2] = VectorTimesScalar(fields[2], 2);
-    fields[0] = VectorPlusVector(fields[0], fields[1]);
-    fields[0] = VectorPlusVector(fields[0], fields[2]);
-    fields[0] = VectorPlusVector(fields[0], fields[3]);
-    fields[0] = VectorTimesScalar(fields[0], h_six);
+    fields[1] = VectorTimesScalar(fields[1], 2);           //f(y,t) -> 2*f(y,t)                            
+    fields[2] = VectorTimesScalar(fields[2], 2);           //f(z,t) -> 2*f(z,t)
+    fields[0] = VectorPlusVector(fields[0], fields[1]);    //f(x(0),t) + 2*f(y,t)                         
+    fields[0] = VectorPlusVector(fields[0], fields[2]);    //f(x(0),t) + 2*f(y,t) + 2*f(z,t)
+    fields[0] = VectorPlusVector(fields[0], fields[3]);    //f(x(0),t) + 2*f(y,t) + 2*f(z,t) + f(w,t)
+    fields[0] = VectorTimesScalar(fields[0], h_six);       //[f(x(0),t) + 2*f(y,t) + 2*f(z,t) + f(w,t)]*h/6
 
-    result = VectorPlusVector(values, fields[0]);
+    result = VectorPlusVector(values, fields[0]);          //x(0) + [f(x(0),t) + 2*f(y,t) + 2*f(z,t) + f(w,t)]*h/6                               
     return result;
 }
