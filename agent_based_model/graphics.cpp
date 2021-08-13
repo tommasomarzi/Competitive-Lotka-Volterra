d275cdc28368c59e906acd6be5e31b6d547e9964
Tommaso Marzi <marzitommaso@gmail.com>
2021-08-13 17:30:32 +0200

    Added doxygen documentation to graphics.cpp and graphics.h files

--- agent_based_model/graphics.cpp
+++ agent_based_model/graphics.cpp
@@ -1,8 +1,14 @@
 #include "graphics.h"
 
+
 extern LV *simulation;
 bool pause = false;
 
+
+/**
+ * It creates a window with size defined by the parameters w_width and w_height of the setup.py, specific
+   position and title. 
+ */
 void create_window(void)
 {
 	glutInitWindowSize(w_width, w_height);                   //set window size 
@@ -11,17 +17,31 @@ void create_window(void)
 	glClearColor(1, 1, 1, 1);                                //values for the color buffers
 }
 
-void reshape(int width, int height) 
+
+/**
+ * This function will be called by the glutReshapeFunc, which requires as argument: void (*func)(int width, int height).
+ * For more information about how glutReshapeFunc works, check the official documentation: https://www.opengl.org/resources/libraries/glut/spec3/node48.html
+ * @param width the new width (in pixels) of the windows. By default, this value is set to w_width.
+ * @param height the new height (in pixels) of the windows. By default, this value is set to w_height.
+ */
+void reshape(int width = w_width, int height = w_height) 
 {
-	glViewport(0, 0, w_width, w_height);          //transformation from device coords to window coords
+	cout<<"hey"<<endl;
+	glViewport(0, 0, width, height);          	  // transformation from device coords to window coords
 	glMatrixMode(GL_PROJECTION);                  // Select The Projection Matrix
 	glLoadIdentity();                             // Reset  The Projection Matrix
-	gluOrtho2D(w_left, w_right, w_down, w_up);    //corners
+	gluOrtho2D(w_left, w_right, w_down, w_up);    // corners
 	glMatrixMode(GL_MODELVIEW);                   // Select The Modelview Matrix
 	glLoadIdentity();                             // Reset  The Modelview Matrix
-	glutPostRedisplay();                          //marks the current window as needing to be redisplayed
 }
 
+
+/**
+ * This function will be called by the glutDisplayFunc, which requires as argument: void (*func)(void).
+ * In particular, it draws each cell of the grid with the associated color. 
+ * 
+ * For more information about how glutDisplayFunc works, check the official documentation: https://www.opengl.org/resources/libraries/glut/spec3/node46.html
+ */
 void display(void) 
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);   // Clear  The Screen And The Depth Buffer
@@ -48,17 +68,43 @@ void display(void)
 }
 
 
+/**
+ * Once the grid is updated, it calls the glutPostRedisplay function (https://www.opengl.org/resources/libraries/glut/spec3/node20.html),
+   which sets the current window to be redisplayed.
+ *
+ * The update function is called recursively by glutTimerFunc function (https://www.opengl.org/resources/libraries/glut/spec3/node64.html) and
+   therefore the following shape is required: void (*func)(int value).
+ * @param value identifier of the callback.
+ */
 void update(int value) 
 {
 	glutKeyboardFunc(keypress);
 	if(!pause)
 	{
 		simulation->evolve();
-		glutPostRedisplay();							//marks the current window to be redisplayed
+		glutPostRedisplay(); 
 	}
 	glutTimerFunc(1000 / fps, update, 0);				//registers a timer callback to be triggered in a number of ms
 }
 
+
+/**
+ * This function is called by the glutKeyboardFunc (https://www.opengl.org/resources/libraries/glut/spec3/node49.html),
+ * which sets the keyboard callback.
+ * The following options for the parameter key are available:
+ * 
+ * 'e': stop the simulation and exit.
+ * 
+ * 'p': stop/resume the simulation.
+ * 
+ * 'i': print the current iteration.
+ * 
+ * 's': print the values of the species w.r.t. their carrying capacities.	
+ * 
+ * @param key chosen keyboard character.
+ * @param x mouse x coordinate (useless in this context but necessary for the glutKeyboardFunc call).
+ * @param y mouse y coordinate (useless in this context but necessary for the glutKeyboardFunc call). 
+ */
 void keypress(unsigned char key, int x, int y) 
 {                                                                              
     switch(key) 
--- agent_based_model/graphics.h
+++ agent_based_model/graphics.h
@@ -5,10 +5,10 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
 
-void create_window(void);
-void reshape(int, int);
-void display(void);
-void update(int);
-void keypress(unsigned char, int, int);
+void create_window(void);                   //!< Create the window that will display the graphics.
+void reshape(int, int);                     //!< Reshape callback function.
+void display(void);                         //!< Display callback function.
+void update(int);                           //!< Update the window after evolving the simulation.
+void keypress(unsigned char, int, int);     //!< Handle the help menu in the graphical part.
 
 #endif //GRAPHICS_H
